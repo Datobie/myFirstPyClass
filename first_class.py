343c9afaa39ca5444409e1af9241734bf28e1d54
Tobiloba Abiodun <abiodunzay@gmail.com>
2022-09-28 04:50:22 -0700

    first time commit

---
+++ first_class.py
@@ -0,0 +1,68 @@
+# boy = "daniel"
+
+# print(boy)
+
+# my_num = 3
+
+# print(my_num)
+
+# float_num = 14.9
+# print(float_num)
+
+# my_num2 = 9
+# print(my_num2/my_num)
+
+# list = "day " "night " "clear"
+# print(list)
+
+
+#classwork
+# a=4
+# b=10
+
+# x = 4*b -2*a
+# print(x)
+
+
+#calculating mean with python
+
+# letNum = 2+4+9+8+7+2
+# average = (letNum/6)
+# print(average)
+
+
+
+# a = 4
+# b = 7
+# c =2
+
+# numerator = (-b) + (4*a*c)**0.5
+# denominator = 2*a
+
+# x = numerator / denominator
+# print(x)
+
+
+# #home
+
+# new_num = 6+10+12+16
+# new_average = new_num/4
+# print(new_average)
+
+# one_minute = 60
+# fourty_two_min = 42
+# num_of_sec = one_minute * fourty_two_min + 42
+
+# print(num_of_sec)
+
+# one_mile = 1.61
+# ten_kilometers = 10 * one_mile
+
+# print(ten_kilometers)
+
+boy_name = "Daniel"
+
+print(boy_name)
+
+
+
